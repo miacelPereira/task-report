1961186856ec5c2afd483c61061c6cdbbb8849fd
miacelPereira <you@example.com>
2020-02-14 21:27:07 -0300

    Creating routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,30 @@
 import express from "express";
-import { config } from "dotenv";
 import cors from "cors";
+import { config } from "dotenv";
+import routes from './routes';
 
-config();
+class App {
+  public express : express.Application;
 
-export default express().use([
-  cors,
-  express.json,
-  express.urlencoded({ extended: true })
-]);
+  public constructor() {
+    this.express = express();
+    this.dontenv();
+    this.middlewares();
+    this.routes();
+  }
+
+  private middlewares() : void {
+    this.express.use(express.json());
+    this.express.use(cors());
+  }
+
+  private routes() : void {
+    this.express.use(routes);
+  }
+
+  private dontenv() : void {
+    config();
+  }
+}
+
+export default new App().express
