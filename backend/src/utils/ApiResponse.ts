21c391be659cd378d575d7af5bbb87daccc9079a
Rafael Oliveira Pinheiro <rafael_oliveira05@hotmail.com.br>
2020-02-16 14:19:58 -0300

    updated ApiResponse class

--- backend/src/utils/ApiResponse.ts
+++ backend/src/utils/ApiResponse.ts
@@ -1,6 +1,14 @@
-export function ApiResponse<T>(message: string, payload: T) {
+export function ApiResponse<T>(
+  message: API_RESPONSE_MESSAGE | string,
+  payload: T
+) {
   return {
     message,
     payload
   };
+}
+
+export enum API_RESPONSE_MESSAGE {
+  VALIDATION_ERROR = "Error on request params",
+  REQUEST_SUCCESSFUL = "Request successful"
 }
